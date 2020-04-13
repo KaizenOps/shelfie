6728bde138ba7d05b9c74222ceb844ad1e232046
KaizenOps <alxndr.mfg@gmail.com>
2020-04-13 14:04:20 -0600

    Initial Commit

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react"
+// import  axios from "axios"
+import Product from "./Product.js"
+
+class Dashboard extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+        };
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+
+};
+
+export default Dashboard
